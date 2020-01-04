833dcb64c50ffec6c7711465fd66938ab9f55fca
fsuffieldcode <fsuffieldcode@gmail.com>
2020-01-04 00:00:29 +0000

    fix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(uri, {useNewUrlParser: true})
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const itemsSchema = {
   name: String
