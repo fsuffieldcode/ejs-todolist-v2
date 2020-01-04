a034de3da093ae1e4418ab80372cc311863c8113
fsuffieldcode <fsuffieldcode@gmail.com>
2020-01-04 00:08:57 +0000

    fix

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-async function connectToDb() {
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-}
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const itemsSchema = {
   name: String
