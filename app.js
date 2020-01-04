c43531f78d683ea49d2cff7c6135e19fcb5d505f
fsuffieldcode <fsuffieldcode@gmail.com>
2020-01-04 00:01:43 +0000

    fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose")
 require('dotenv').config();
 const _ = require("lodash")
 mongoPw = process.env.MONGO_PW
-const uri = 'mongodb+srv://fabian:' + mongoPw + '@cluster0-26kcr.mongodb.net/todolistDB?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://fabian:' + mongoPw + '@cluster0-26kcr.mongodb.net/test?retryWrites=true&w=majority'
 
 
 const app = express();
